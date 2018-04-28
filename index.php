52615fedfcfbf652b745ab6918116ad854a30c0d
Nikitaxub <38690643+Nikitaxub@users.noreply.github.com>
2018-04-28 12:23:41 +0300

    категории и лоты динамически из массива

--- index.php
+++ index.php
@@ -82,49 +82,78 @@ $user_avatar = 'img/user.jpg';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
+			<?php 
+				$lotsList = [
+					0 => [
+						'title' => '2014 Rossignol District Snowboard',
+						'category' => 'Доски и лыжи',
+						'cost' => '10999',
+						'image' => 'img/lot-1.jpg'
+					],
+					1 => [
+						'title' => 'DC Ply Mens 2016/2017 Snowboard',
+						'category' => 'Доски и лыжи',
+						'cost' => '159999',
+						'image' => 'img/lot-2.jpg'
+					],
+					2 => [
+						'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+						'category' => 'Крепления',
+						'cost' => '8000',
+						'image' => 'img/lot-3.jpg'
+					],
+					3 => [
+						'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+						'category' => 'Ботинки',
+						'cost' => '10999',
+						'image' => 'img/lot-4.jpg'
+					],
+					4 => [
+						'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+						'category' => 'Одежда',
+						'cost' => '7500',
+						'image' => 'img/lot-5.jpg'
+					],
+					5 => [
+						'title' => 'Маска Oakley Canopy',
+						'category' => 'Разное',
+						'cost' => '5400',
+						'image' => 'img/lot-6.jpg'
+					]
+				];
+			foreach($lotsList as $lot_id => $lot) {	echo '
+				<li class="lots__item lot">
+					<div class="lot__image">
+						<img src="'.$lot['image'].'" width="350" height="260" alt="Сноуборд">
+					</div>
+					<div class="lot__info">
+						<span class="lot__category">'.$lot['category'].'</span>
+						<h3 class="lot__title"><a class="text-link" href="lot.html">'.$lot['title'].'</a></h3>
+						<div class="lot__state">
+							<div class="lot__rate">
+								<span class="lot__amount">Стартовая цена</span>
+								<span class="lot__cost">'.$lot['cost'].'<b class="rub">р</b></span>
+							</div>
+							<div class="lot__timer timer">
 
-                        </div>
-                    </div>
-                </div>
-            </li>
+							</div>
+						</div>
+					</div>
+			</li>';}?>            
         </ul>
     </section>
 </main>
 
 <footer class="main-footer">
     <nav class="nav">
-        <ul class="nav__list container">
-            <li class="nav__item">
-                <a href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Крепления</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Одежда</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="nav__item">
-                <a href="all-lots.html">Разное</a>
-            </li>
+		<ul class="nav__list container">
+		<?php $itemList = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+			for ($i = 0; $i < count($itemList); $i++) {
+				echo '
+				<li class="nav__item">
+					<a href="all-lots.html">'.$itemList[$i].'</a>
+				</li>';
+			}?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
