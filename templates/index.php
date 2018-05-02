3ea72bee1a67a566eeadd0e7e7e85878a1264e8a
Nikitaxub <38690643+Nikitaxub@users.noreply.github.com>
2018-05-02 21:58:45 +0300

    Добавил шаблонизатор+шаблоны

--- templates/index.php
+++ templates/index.php
@@ -32,15 +32,15 @@
             ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?php echo $lot['image']?>" width="350" height="260" alt="Сноуборд">
+                    <img src="<?= htmlspecialchars($lot['image']); ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?php echo $lot['category']?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?php echo $lot['title']?></a></h3>
+                    <span class="lot__category"><?= htmlspecialchars($lot['category']); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?= htmlspecialchars($lot['title']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?php echo formatCost($lot['cost'])?></span>
+                            <span class="lot__cost"><?= formatCost(htmlspecialchars($lot['cost'])); ?></span>
                         </div>
                         <div class="lot__timer timer">
 
